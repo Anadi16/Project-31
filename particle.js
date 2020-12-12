ce444bb60b09863af3bfdd706f16c704c22581ab
Anadi16 <71920124+Anadi16@users.noreply.github.com>
2020-12-12 11:30:28 +0530

    Add files via upload

---
+++ particle.js
@@ -0,0 +1,30 @@
+class Particle{
+    constructor(x,y,radius){
+
+        var options = {
+            isStatic : false,
+            restitution : 0.4
+            
+        }
+        
+        
+        this.body = Bodies.circle(x,y,radius,options);
+        this.color = color(random(0,255), random(0,255), random(0,255));
+        this.radius = radius;
+
+        World.add(world,this.body);
+    }
+    display(){
+
+        var pos = this.body.position;
+        var angle = this.body.angle;
+
+
+      
+        ellipseMode(RADIUS);
+            fill(this.color);
+        ellipse(pos.x,pos.y,this.radius,this.radius);
+        
+    }
+
+}
